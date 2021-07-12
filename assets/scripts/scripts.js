40c78ad2a7885693f7deb01630516e21aea66828
iamkiambuthi <kiambuthikinuthia@gmail.com>
2021-07-12 13:09:39 +0300

    coolors

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,10 @@
 /** landing-page scripts.js | MIT LICENSE 2021 | kyambuthi */
+
+const clientEmail = document.getElementById('emailInput');
+
+clientEmail.addEventListener('input', function(e){
+    console.log(e);
+    if(clientEmail.validity.typeMismatch) {
+        clientEmail.setCustomValidity('please enter a valid email address.');
+    }
+});
